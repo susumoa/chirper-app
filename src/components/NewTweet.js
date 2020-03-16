326d9e0acb7e46a335bbf8fd1ba16b1375b5c086
susumoa <tobiasbori@gmail.com>
2020-03-16 10:42:13 +0000

    feat: New Tweet logic

--- src/components/NewTweet.js
+++ src/components/NewTweet.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { handleAddTweet } from '../actions/tweets'
 
 class NewTweet extends Component {
   state = {
@@ -15,9 +17,9 @@ class NewTweet extends Component {
     e.preventDefault()
 
     const { text } = this.state
+    const { dispatch, id } = this.props
 
-    // todo: Add Tweet to Store
-    console.log('New Tweet: ', text)
+    dispatch(handleAddTweet(text, id))
 
     this.setState(() => ({
       text: ''
@@ -25,6 +27,8 @@ class NewTweet extends Component {
   }
   render() {
     const { text } = this.state
+
+    // todo: Redired to / if submitted
 
     const tweetLeft = 280 - text.length
 
@@ -57,4 +61,4 @@ class NewTweet extends Component {
   }
 }
 
-export default NewTweet
+export default connect()(NewTweet)
